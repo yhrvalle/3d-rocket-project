22a448c9afcb0601fc5306714490c43ed28cd556
yhr <danvalleribeiro@gmail.com>
2026-02-19 11:11:26 -0300

    Scene Management and more empty scene

--- Assets/_Project/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/_Project/Scripts/Player/PlayerCollisionHandler.cs
@@ -1,7 +1,11 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    private static WaitForSeconds _waitForSeconds1 = new(1f);
+
     private void OnCollisionEnter(Collision collision)
     {
         switch (collision.gameObject.tag)
@@ -11,10 +15,29 @@ public class PlayerCollisionHandler : MonoBehaviour
                 break;
             case "Finish":
                 Debug.Log("You win!");
+                StartCoroutine(LoadNextLevel());
                 break;
             default:
                 Debug.Log("You lose!");
+                StartCoroutine(ReloadLevel());
                 break;
         }    
+    }
+
+    private IEnumerator ReloadLevel()
+    {
+        yield return _waitForSeconds1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private IEnumerator LoadNextLevel()
+    {
+        yield return _waitForSeconds1;
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
